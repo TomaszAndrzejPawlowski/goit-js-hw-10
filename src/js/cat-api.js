3c107f19b638ccc38bf3076e5ce5a1f00340acaf
Tomasz <pawlowski.t.93@gmail.com>
2023-09-21 18:13:03 +0200

    Transfered most of the code from cat-api.js to index.js

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -3,70 +3,23 @@ import Notiflix, { Notify } from 'notiflix';
 import axios from 'axios';
 axios.defaults.headers.common['x-api-key'] =
   'live_mkuMlJP9LBmkoRnjHtQH8p26XSUgTGsoO6qrSlitn7hlHbp0ZRqd8QRFn7m12sDX';
-import SlimSelect from 'slim-select';
 
-const selectOptions = document.querySelector('.breed-select');
 const errorMsg = document.querySelector('.error');
-const catInfoDiv = document.querySelector('.cat-info');
-const loadingTxt = document.querySelector('.loader');
-
-const loadingScreen = document.createElement('div');
-loadingScreen.classList.add('body-background');
 
 export const fetchBreeds = () => {
-  selectOptions.before(loadingScreen);
-  loadingTxt.classList.remove('is-hidden');
   return axios
     .get('https://api.thecatapi.com/v1/breeds')
     .then(response => {
-      console.log(response.data);
       return response.data;
-    })
-    .then(breeds => {
-      const markup = breeds.map(breed => {
-        return `<option value="${breed.id}">${breed.name}</option>`;
-      });
-      selectOptions.innerHTML = markup;
-      new SlimSelect({
-        select: selectOptions,
-        settings: catInfoDiv,
-      });
-      document.querySelector('.body-background').remove();
-      loadingTxt.classList.add('is-hidden');
     })
     .catch(error => Notify.failure(errorMsg.textContent));
 };
 
 export const fetchCatByBreed = breedId => {
-  selectOptions.before(loadingScreen);
-  loadingTxt.classList.remove('is-hidden');
-  if (document.querySelector('.cat-img')) {
-    document.querySelector('.cat-img').remove();
-  }
-  const selectedOptionValue = breedId.currentTarget.value;
   return axios
-    .get(
-      `https://api.thecatapi.com/v1/images/search?breed_ids=${selectedOptionValue}`
-    )
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
     .then(response => {
-      console.log(response.data);
       return response.data;
-    })
-    .then(cat => {
-      const markup = `
-              <div class="cat-img"><img
-              src=${cat[0].url} 
-              alt="Photo of a ${cat[0].breeds[0].name} cat" 
-              width= "600px"
-              height= "30%"/>
-              <div class="cat-desc">
-              <h2>${cat[0].breeds[0].name}</h2>
-              <p>${cat[0].breeds[0].description}</p>
-              <p><strong>Temperament:</strong> ${cat[0].breeds[0].temperament}</p></div></div>
-              `;
-      document.querySelector('.body-background').remove();
-      loadingTxt.classList.add('is-hidden');
-      catInfoDiv.insertAdjacentHTML('afterbegin', markup);
     })
     .catch(error => Notify.failure(errorMsg.textContent));
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,62 @@
 `use strict`;
+import Notiflix, { Notify } from 'notiflix';
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
+import SlimSelect from 'slim-select';
 
 const selectOptions = document.querySelector('.breed-select');
+const loadingTxt = document.querySelector('.loader');
+const catInfoDiv = document.querySelector('.cat-info');
+const errorMsg = document.querySelector('.error');
 
-fetchBreeds();
-selectOptions.addEventListener('change', fetchCatByBreed);
+const loadingScreen = document.createElement('div');
+loadingScreen.classList.add('body-background');
+
+const loadApp = () => {
+  selectOptions.before(loadingScreen);
+  loadingTxt.classList.remove('is-hidden');
+  fetchBreeds()
+    .then(breeds => {
+      const markup = breeds.map(breed => {
+        return `<option value="${breed.id}">${breed.name}</option>`;
+      });
+      selectOptions.innerHTML = markup;
+      new SlimSelect({
+        select: selectOptions,
+        settings: catInfoDiv,
+      });
+      document.querySelector('.body-background').remove();
+      loadingTxt.classList.add('is-hidden');
+    })
+    .catch(error => Notify.failure(errorMsg.textContent));
+};
+
+const showChosenCat = event => {
+  selectOptions.before(loadingScreen);
+  loadingTxt.classList.remove('is-hidden');
+  if (document.querySelector('.cat-img')) {
+    document.querySelector('.cat-img').remove();
+  }
+  const selectedOptionValue = event.currentTarget.value;
+  fetchCatByBreed(selectedOptionValue)
+    .then(cat => {
+      const markup = `
+              <div class="cat-img"><img
+              src=${cat[0].url} 
+              alt="Photo of a ${cat[0].breeds[0].name} cat" 
+              width= "600px"
+              height= "30%"/>
+              <div class="cat-desc">
+              <h2>${cat[0].breeds[0].name}</h2>
+              <p>${cat[0].breeds[0].description}</p>
+              <p><strong>Temperament:</strong> ${cat[0].breeds[0].temperament}</p></div></div>
+              `;
+      document.querySelector('.body-background').remove();
+      loadingTxt.classList.add('is-hidden');
+      catInfoDiv.insertAdjacentHTML('afterbegin', markup);
+    })
+    .catch(error => Notify.failure(errorMsg.textContent));
+};
+
+loadApp();
+
+selectOptions.addEventListener('change', showChosenCat);
